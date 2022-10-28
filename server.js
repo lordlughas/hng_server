00b5a0d8198b166f439b71d2f3028b011a17889f
Charles Lughas <lordlughas@gmail.com>
2022-10-28 14:43:16 +0100

    Added cors dependencies

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-//jshint esversion:6
-
 const express = require("express");
 
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3030;
 // app.get("/", function(req,res){
 //     res.send("Hello World");
 // });
 
+app.use(cors());
 app.get("/", function(req, res){
     res.json({
         "slackUsername": "clughas", 
